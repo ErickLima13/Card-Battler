5db5f9bfb51eb3df34444d4d3689720f2c37be3f
ErickAraujo <erickequipe7@gmail.com>
2026-07-09 18:18:04 -0300

    add : hover behavior

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Card : MonoBehaviour
 {
+    private Vector3 _originalScale;
+    private Vector3 _originalPosition;
+
+    private int _originalSortOrder;
+
     [SerializeField] private SpriteRenderer _illustrationRender;
 
     [SerializeField] private TextMeshPro _cardName;
@@ -11,9 +17,17 @@ public class Card : MonoBehaviour
 
     [SerializeField] private CardData tempData;
 
+    [SerializeField] private SortingGroup _sortingGroup;
+
+    [SerializeField] private float _hoverScale = 2;
+    [SerializeField] private float _hoverOffset = 3;
 
     private void Start()
     {
+        _originalSortOrder = _sortingGroup.sortingOrder;
+        _originalPosition = transform.position;
+        _originalScale = transform.localScale;
+
         LoadCardData(tempData);
     }
 
@@ -23,5 +37,22 @@ public class Card : MonoBehaviour
         _cardName.text = data.cardName;
         _description.text = data.description;
         _actionCost.text = data.actionCost.ToString();
+    }
+
+    private void OnMouseEnter()
+    {
+        print("mouse enter");
+        transform.localScale = _originalScale * _hoverScale;
+        transform.localPosition += new Vector3(0,_hoverOffset,0);
+        _sortingGroup.sortingOrder += 1;
+    }
+
+    private void OnMouseExit()
+    {
+        print("mouse exit");
+
+        transform.localScale = _originalScale;
+        transform.localPosition = _originalPosition;
+        _sortingGroup.sortingOrder = _originalSortOrder;
     }
 }
