61b67939cb6f9feaeab6cf781bbcbe50d7b1784c
ErickAraujo <erickequipe7@gmail.com>
2026-07-09 14:53:10 -0300

    add : card and deck scripts

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardData", menuName = "Scriptable Objects/CardData")]
+public class CardData : ScriptableObject
+{
+    public string cardName;
+    public string description;
+
+    public int actionCost;
+    public int attackPower;
+    public int healPower;
+
+    public Sprite illustration;
+
+
+}
